8f559bdf600aab4fbec62b5643a2397bccd3ad81
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-10-24 11:45:05 +0530

    db connection issue resolved

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined;
+};
+
+const prismaClient = new PrismaClient({
+  log: ["query"],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prismaClient;
 }
 
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query'],
-    })
+function getPrismaClient() {
+  if (process.env.NODE_ENV === "production") {
+    const url = process.env.DATABASE_URL;
+    if (!url) {
+      throw new Error("DATABASE_URL is not defined");
+    }
+    const newUrl = `${url}?pgbouncer=true&pool_timeout=10`;
+    return new PrismaClient({
+      datasources: {
+        db: {
+          url: newUrl,
+        },
+      },
+    });
+  }
+  return globalForPrisma.prisma ?? prismaClient;
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = getPrismaClient();
