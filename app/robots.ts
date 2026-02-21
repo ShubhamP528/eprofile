7a3fa6f6a12bbee84e04d6c4355203aab13be90d
Shubham Prajapati <shubham528prajapati@gmail.com>
2026-02-21 17:54:28 +0530

    robot.txt edited

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+            {
+                userAgent: ['GPTBot', 'ChatGPT-User', 'CCBot', 'ImagesiftBot', 'OAI-SearchBot', 'Amazonbot', 'Applebot-Extended', 'Bytespider'],
+                disallow: '/',
+            }
+        ],
         sitemap: 'https://eprofile.cv/sitemap.xml',
     }
 }
