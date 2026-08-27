0bfc95248d6ea3d12bd69572dd88b0a5701094dd
Shubham Prajapati <shubham528prajapati@gmail.com>
2026-08-27 07:16:04 +0530

    new changes done

--- app/dashboard/layout-client.tsx
+++ app/dashboard/layout-client.tsx
@@ -103,7 +103,7 @@ export default function DashboardLayoutClient({
       )}
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden transition-all duration-300">
+      <div className="flex-1 flex flex-col w-full min-w-0 overflow-hidden transition-all duration-300">
         <DashboardHeader
           toggleMobileMenu={toggleMobileMenu}
           toggleSidebar={toggleSidebar}
